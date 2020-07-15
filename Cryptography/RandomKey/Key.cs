b16253e4bd28c73acc77db558408900d77398643
Anderson Simioni <anderson.simioni22@gmail.com>
2020-07-15 02:53:29 -0300

    RandomKeyMap
    Rebuild Key {
    -Universal name
    -Crypted data
    -Add crypted password
    -Add expiration date
    }

--- Cryptography/RandomKey/Key.cs
+++ Cryptography/RandomKey/Key.cs
@@ -1,72 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using Cryptographic;
 using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Text.Json.Serialization;
 
-namespace hasbots.Classes.Cryptographic.RandomKeyMap
+namespace Cryptography.RandomKey
 {
     /// <summary>
-    /// This class represent a key with data linked
+    /// <DEVELOPER> Anderson Simioni </DEVELOPER>
+    /// Random map key that will be a random name 
+    /// generated and store values.
     /// </summary>
     class Key
     {
-        private readonly static char[] printables = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890").ToCharArray();
+        [JsonProperty("Name")]
+        private readonly string Name;
+
+        [JsonProperty("Password")]
+        private readonly string Password;
+
+        [JsonProperty("Data")]
+        private readonly string Data;
+
+        [JsonProperty("RegisterDate")]
+        private readonly DateTime RegisterDate;
+
+        [JsonProperty("ExpirationDate")]
+        private readonly DateTime ExpirationDate;
 
         /// <summary>
-        /// Name for call this key
+        /// Try acess key data with password
         /// </summary>
-        public string Name { get; }
-
-        /// <summary>
-        /// data value of this key
-        /// </summary>
-        public string Value { get; }
-
-        /// <summary>
-        /// Date of creation this key
-        /// </summary>
-        public DateTime CreationDate { get; }
-
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="data">data for link to this key</param>
-        [Newtonsoft.Json.JsonConstructor]
-        public Key(string name, string value, DateTime creationDate) 
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public (bool result, string data) getData(string password = "") 
         {
-            if (string.IsNullOrEmpty(value))
-                throw new Exception("Null or empty key value!");
+            var accessAccepted = 
+                string.IsNullOrEmpty(this.Password) || 
+                (this.Password == Base64.crypt(password));
 
-            if (string.IsNullOrEmpty(name))
-                throw new Exception("Name is null or empty!");
+            var expired = ExpirationDate == null;
+            if (expired == false)
+                expired = (ExpirationDate - DateTime.Now).TotalSeconds <= 0;
 
-            if (creationDate == null)
-                throw new Exception("Creation date is invalid!");
+            if (accessAccepted) 
+            {
+                var decryptedData = Base64.decrypt(Data);
+                return (true, decryptedData);
+            }
 
-            this.Name = name;
-            this.Value = value;
-            this.CreationDate = creationDate;
+            return (false, "Access denied");
         }
 
         /// <summary>
-        /// Create a new random key
+        /// Create new random name, it not exist on names list
         /// </summary>
-        /// <param name="data">key value</param>
-        /// <returns>new random key</returns>
-        public static Key createNewRandomKey(string data) 
+        /// <param name="len"></param>
+        /// <param name="existingNames"></param>
+        /// <returns></returns>
+        private static string getNewRandomName(int len, string[] existingNames) 
         {
-            var random = new Random((int)DateTime.Now.Ticks);
+            var newName = string.Empty;
+            var randomGen = new Random(DateTime.Now.Millisecond);
 
-            string key = "";
-            var strong = data.Length * 2 + (int)(random.NextDouble() * data.Length);
+            while (newName == string .Empty || existingNames.Contains(newName))
+            {
+                newName = "";
 
-            while (key.Length < strong)
-                key += printables[(int)(random.NextDouble() * printables.Length)];
+                for (int i = 0; i < len; i++)
+                    newName += (int)((char)randomGen.Next(char.MinValue, char.MaxValue)) + (i == len - 1 ? "" : "x");
 
-            return new Key(key, data, DateTime.Now);
+                newName = Base64.crypt(newName);
+            }
+
+            return newName;
+        }
+
+        /// <summary>
+        /// Create new random key with new name and
+        /// save data and password, set expiration date 
+        /// to null for not have
+        /// </summary>
+        /// <param name="existingNames"></param>
+        /// <param name="data"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static Key create(string[] existingNames, string data, DateTime expirationDate, string password = "") 
+        {     
+            var cryptedData = Base64.crypt(data);
+            var cryptedPassword = Base64.crypt(password);
+            var name = getNewRandomName(data.Length, existingNames);
+
+            return new Key(name, cryptedPassword, cryptedData, DateTime.Now, expirationDate);
+        }
+
+        [JsonConstructor]
+        private Key(string Name, string Password, string Data, DateTime RegisterDate, DateTime ExpirationDate) 
+        {
+            if (string.IsNullOrEmpty(Name))
+                throw new Exception("Name is empty or null!");
+
+            this.Name = Name;
+            this.Password = Password;
+            this.Data = Data;
+            this.RegisterDate = RegisterDate;
+            this.ExpirationDate = ExpirationDate;
         }
     }
 }
